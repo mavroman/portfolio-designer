dcd9a717715d39b7a04b3331735d367fe14c87f3
Roman Mavrin <roma-mrv@yandex.ru>
2020-11-30 01:58:08 +0300

    added fonts

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const menuIcon = document.querySelector('.menu-icon');
-const menu = document.querySelector('.menu');
-
-// const menuIconExit = document.querySelector('.menu-icon_active');
+const menuIcon = document.querySelector(".menu-icon");
+const menu = document.querySelector(".menu");
+const menuItem = document.querySelectorAll(".menu__link");
 
 function addMenu() {
-    menuIcon.classList.toggle('menu-icon_active');
-    menu.classList.toggle('menu_active');
+  menuIcon.classList.toggle("menu-icon_active");
+  menu.classList.toggle("menu_active");
 }
 
-menuIcon.addEventListener('click', addMenu)
+menuIcon.addEventListener("click", addMenu);
+
+menuItem.forEach(function (e) {
+  e.addEventListener("click", function () {
+    addMenu();
+  });
+});
